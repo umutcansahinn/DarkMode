e0561280a0e721ddb79dfc7ffd7ee7ef8c7e864d
Umutcan Sahin <umutcan199726@gmail.com>
2023-06-28 23:21:48 +0300

    if dark mode is active, turn on the switch

--- app/src/main/java/com/umutcansahin/setdarkmodewithbutton/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/umutcansahin/setdarkmodewithbutton/ui/notifications/NotificationsFragment.kt
@@ -1,9 +1,11 @@
 package com.umutcansahin.setdarkmodewithbutton.ui.notifications
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.umutcansahin.setdarkmodewithbutton.ThemeManager
 import com.umutcansahin.setdarkmodewithbutton.databinding.FragmentNotificationsBinding
@@ -30,8 +32,24 @@ class NotificationsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        changeUiMode()
+        setSwitchWithUiMode()
+    }
+
+    private fun changeUiMode() {
         binding.switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
             themeManager.setUiTheme(isChecked.not())
+        }
+    }
+
+    private fun setSwitchWithUiMode() {
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                binding.switchDarkMode.isChecked = false
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                binding.switchDarkMode.isChecked = true
+            }
         }
     }
 
